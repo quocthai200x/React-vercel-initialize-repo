c578a5e6a39805f493133aa24a83dc358aa25e6c
quocthai200x <quocthai2000x@gmail.com>
2024-10-07 01:01:35 +0700

    test master

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,4 +1,4 @@
 const Test = () => {
-  return <div className="text-sm text-red-500">Some change</div>;
+  return <div className="text-sm text-red-500">Some change master</div>;
 };
 export default Test;
